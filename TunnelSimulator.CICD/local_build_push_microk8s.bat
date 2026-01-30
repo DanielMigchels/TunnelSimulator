288452d6dadca9c2bc8e9e56940fac281a455a1e
Daniel Migchels <danielm12@hotmail.nl>
2026-01-30 13:42:45 +0100

    Remove CICD directory

--- TunnelSimulator.CICD/local_build_push_microk8s.bat
+++
@@ -1,6 +0,0 @@
-cd ..
-docker rmi 127.0.0.1:32000/tunnelsimulator:latest
-docker build -f TunnelSimulator.Docker/dockerfile -t 127.0.0.1:32000/tunnelsimulator:latest .
-docker push 127.0.0.1:32000/tunnelsimulator:latest
-kubectl rollout restart deploy tunnelsimulator-deployment --kubeconfig "C:\Users\daniel\.kube\dev-tools" --namespace=tunnelsimulator
-pause
